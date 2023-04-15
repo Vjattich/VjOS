c37e03977f1e11ccb0fc3680a291db98c6b96186
Vj <sektaaccel@gmail.com>
2023-04-15 15:13:49 +0300

    fix

--- Executables/MOVE_AHK_SCRIPTS_TO_STARTUP.bat
+++ Executables/MOVE_AHK_SCRIPTS_TO_STARTUP.bat
@@ -4,7 +4,7 @@ if exist "ahk-scripts\clipboard.ahk" (
 	move /y "ahk-scripts\clipboard.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\clipboard.ahk"
 )
 if exist "ahk-scripts\three_finger_drag.ahk" (
-	echo move /y "ahk-scripts\clipboard.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
-	move /y "ahk-scripts\clipboard.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
+	echo move /y "ahk-scripts\three_finger_drag.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
+	move /y "ahk-scripts\three_finger_drag.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
 )
 echo "Done"
