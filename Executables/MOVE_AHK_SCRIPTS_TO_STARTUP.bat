e78d2e8f53d3d828edb2f36e65ab8b067a10e002
Vj <sektaaccel@gmail.com>
2023-04-16 15:02:13 +0300

    fix

--- Executables/MOVE_AHK_SCRIPTS_TO_STARTUP.bat
+++ Executables/MOVE_AHK_SCRIPTS_TO_STARTUP.bat
@@ -7,4 +7,8 @@ if exist "ahk-scripts\three_finger_drag.ahk" (
 	echo move /y "ahk-scripts\three_finger_drag.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
 	move /y "ahk-scripts\three_finger_drag.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\three_finger_drag.ahk"
 )
+if exist "ahk-scripts\lang_switch.ahk" (
+	echo move /y "ahk-scripts\lang_switch.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\lang_switch.ahk"
+	move /y "ahk-scripts\lang_switch.ahk" "%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup\lang_switch.ahk"
+)
 echo "Done"
