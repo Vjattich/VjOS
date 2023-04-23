06cf7fc4e0636a65bd9bc092b1ccec3dbef5df4c
Vj <sektaaccel@gmail.com>
2023-04-23 13:24:59 +0300

    test

--- Executables/unpin_edge.ps1
+++ Executables/unpin_edge.ps1
@@ -28,9 +28,9 @@ echo "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
 echo "$([WinAPI.GetStr]::GetString(5387))"
 # Call the shortcut context menu item
 cd "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
-ls
 
 $Shell = (New-Object -ComObject Shell.Application).NameSpace("$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar")
 $Shortcut = $Shell.ParseName("Microsoft Edge.lnk")
 # Extract the localized "Unpin from taskbar" string from shell32.dll
 $Shortcut.Verbs() | Where-Object -FilterScript {$_.Name -eq "$([WinAPI.GetStr]::GetString(5387))"} | ForEach-Object -Process {$_.DoIt()}
+$Shortcut.Verbs() | Where-Object -FilterScript {$_.Name -eq "Unpin from taskbar"} | ForEach-Object -Process {$_.DoIt()}
