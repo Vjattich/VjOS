9c0019f5307a554b2a7b02d4e33acb3d4d7787d4
Vj <sektaaccel@gmail.com>
2023-05-09 17:49:20 +0300

    fix

--- package.ps1
+++ package.ps1
@@ -2,4 +2,4 @@ git pull
 if (Test-Path -Path '.\VjOS.apbx' -PathType Leaf) {
      rm '.\VjOS.apbx'
 }
-& 'C:\Program Files\7-Zip\7z.exe' a -tzip -mx5 -r0 '.\VjOS.apbx' '.\'
+& 'C:\Program Files\7-Zip\7z.exe' a -tzip -mx5 -r0 '.\VjOS.apbx' '.\Configuration' '.\Executables' '.\Localizations' '.\Manifest' '.\Module' '.\playbook.conf' '.\playbook.png'
